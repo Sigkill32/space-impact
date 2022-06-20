a31fa3264d4695bcd1a52a728fb50a99b0841519
Manoj Bhat <bhatanm@gmail.com>
2022-06-20 14:42:22 +0530

    Move ship to position

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const SHIP_WIDTH = 15;
 export const SHIP_HEIGHT = 15;
+export const CANVAS_PADDING = 50;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import { SHIP_WIDTH, SHIP_HEIGHT } from "./constants";
+import { SHIP_WIDTH, SHIP_HEIGHT, CANVAS_PADDING } from "./constants";
 import renderAllChars from "./components";
+import Observer from "./Observer";
 
 const canvas = document.getElementById("gameCanvas");
 const documentHeight = window.innerHeight;
 const documentWidth = window.innerWidth;
 
-canvas.setAttribute("width", documentWidth - 50);
+canvas.setAttribute("width", documentWidth - CANVAS_PADDING);
 canvas.setAttribute("height", Math.floor(documentHeight * 0.8));
 const ctx = canvas.getContext("2d");
 
 const MAX_WIDTH = canvas.width;
 const MAX_HEIGHT = canvas.height;
 
-console.log({ MAX_WIDTH, MAX_HEIGHT, documentHeight, documentWidth });
+const observer = new Observer();
+
+/* -------------------------------variables---------------------------------------*/
+
 const coordinates = {
   myShip: {
     x: Math.floor(MAX_WIDTH / 2),
@@ -28,10 +32,25 @@ const coordinates = {
   },
 };
 
+/* ------------------------------------------------------------------------------*/
+
 renderAllChars(ctx, coordinates);
 
-function handleMove(e) {
-  console.log(e.clientX);
+function steerShip(e) {
+  const { clientX } = e;
 }
 
-document.addEventListener("mousemove", handleMove);
+function handleShipMotionActivation(e) {
+  const { type, clientX } = e;
+  if (type === "mousedown") {
+    coordinates.myShip.x = clientX - CANVAS_PADDING / 2;
+    renderAllChars(ctx, coordinates);
+    canvas.addEventListener("mousemove", steerShip);
+  }
+  if (type === "mouseup") {
+    canvas.removeEventListener("mousemove", steerShip);
+  }
+}
+
+canvas.addEventListener("mousedown", handleShipMotionActivation);
+canvas.addEventListener("mouseup", handleShipMotionActivation);
