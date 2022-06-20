2d72a0bb0644340210c847611a69bbc118f8ca95
Manoj Bhat <bhatanm@gmail.com>
2022-06-20 13:23:06 +0530

    Fixed mobile browser height issues

--- src/index.js
+++ src/index.js
@@ -2,29 +2,36 @@ import { SHIP_WIDTH, SHIP_HEIGHT } from "./constants";
 import renderAllChars from "./components";
 
 const canvas = document.getElementById("gameCanvas");
-const documentHeight = document.body.clientHeight;
-const documentWidth = document.body.clientWidth;
+const documentHeight = window.innerHeight;
+const documentWidth = window.innerWidth;
 
-canvas.setAttribute("width", documentWidth);
-canvas.setAttribute("height", Math.floor(documentHeight * 0.75));
+canvas.setAttribute("width", documentWidth - 50);
+canvas.setAttribute("height", Math.floor(documentHeight * 0.8));
 const ctx = canvas.getContext("2d");
 
-const MAX_WIDTH = canvas.clientWidth;
-const MAX_HEIGHT = canvas.clientHeight;
+const MAX_WIDTH = canvas.width;
+const MAX_HEIGHT = canvas.height;
 
+console.log({ MAX_WIDTH, MAX_HEIGHT, documentHeight, documentWidth });
 const coordinates = {
   myShip: {
     x: Math.floor(MAX_WIDTH / 2),
     y: MAX_HEIGHT - SHIP_WIDTH,
   },
   enemyShip: {
-    x: MAX_WIDTH + 5, // keep the ship away from users view
+    x: MAX_WIDTH + 5, // extra 5 to keep the ship away from users view
     y: 0,
   },
   bullet: {
-    x: Math.floor(MAX_WIDTH / 2),
+    x: MAX_WIDTH / 2,
     y: MAX_HEIGHT - SHIP_WIDTH,
   },
 };
 
 renderAllChars(ctx, coordinates);
+
+function handleMove(e) {
+  console.log(e.clientX);
+}
+
+document.addEventListener("mousemove", handleMove);
